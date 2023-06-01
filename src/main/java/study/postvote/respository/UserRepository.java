dba7bba3c4f27587dc4e2bacafc8cc5966c2ef4a
oddnine <dnwnsgur12@gmail.com>
2023-06-01 17:18:32 +0900

    userRepo ex

--- src/main/java/study/postvote/respository/UserRepository.java
+++ src/main/java/study/postvote/respository/UserRepository.java
@@ -1,6 +1,8 @@
 package study.postvote.respository;
 
 import study.postvote.domain.User;
+import study.postvote.domain.type.City;
+import study.postvote.domain.type.Mbti;
 import study.postvote.respository.db.ConnectionManager;
 
 import java.sql.Connection;
@@ -11,17 +13,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class UserRepository implements UserRepository{
+public class UserRepository {
     Connection conn;
 
-    @Override
-    public void insert(User library) {
-        String sql = "insert into user (name, author, publisher, isbn, release_year, count, summary, image, category) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    public void insert(User user) {
+        String sql = "insert into user (name, age, gender, city, email, password, mbti) values(?, ?, ?, ?, ?, ?, ?)";
         try {
             conn = ConnectionManager.getConnection();
 
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, library.getName());
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, String.valueOf(user.getAge()));
+            pstmt.setString(3, String.valueOf(user.isGender()));
+            pstmt.setString(4, String.valueOf(user.getCity()));
+            pstmt.setString(5, user.getEmail());
+            pstmt.setString(6, user.getPassword());
+            pstmt.setString(7, String.valueOf(user.getMbti()));
 
             pstmt.executeUpdate();
         } catch (SQLException e) {
@@ -36,11 +43,11 @@ public class UserRepository implements UserRepository{
         try {
             rs = pstmt.executeQuery();
 
-//            while (rs.next()) {
-//                libraryList.add(new User(Long.parseLong(rs.getString(1)), rs.getString(2), rs.getString(3),
-//                        rs.getString(4), rs.getString(5), Integer.parseInt(rs.getString(6)), Integer.parseInt(rs.getString(7)),
-//                        rs.getString(8), rs.getString(9), rs.getString(10)));
-//            }
+            while (rs.next()) {
+                libraryList.add(new User(Long.parseLong(rs.getString(1)), rs.getString(2), Integer.parseInt(rs.getString(3)),
+                        Boolean.parseBoolean(rs.getString(4)), City.valueOf(rs.getString(5)), rs.getString(6), rs.getString(7),
+                        Mbti.valueOf(rs.getString(8))));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
