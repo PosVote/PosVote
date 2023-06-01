7bb31a016724143b20cfba52e02b0eb2481069e3
USER <gkstjr0425@naver.com>
2023-06-01 16:35:29 +0900

    Remove cached files

--- src/main/java/study/postvote/domain/Post.java
+++ src/main/java/study/postvote/domain/Post.java
@@ -1,4 +1,42 @@
 package study.postvote.domain;
 
+import java.time.LocalDate;
+
 public class Post {
+    private Long postId;
+    private Long userId;
+    private String title;
+    private String description;
+    private Long voidId;
+    private LocalDate date;
+
+    public Post(Long postId, Long userId, String title, String description, Long voidId, LocalDate date) {
+        this.postId = postId;
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.voidId = voidId;
+        this.date = date;
+    }
+
+    public Post(Long userId, String title, String description, Long voidId, LocalDate date)  {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.voidId = voidId;
+        this.date = date;
+    }
+
+    public Long getPostId() { return postId; }
+
+    public Long getUserId() { return userId; }
+
+    public String getTitle() { return title; }
+
+    public String getDescription() { return description; }
+
+    public Long getVoidId() { return voidId; }
+
+    public LocalDate getDate() { return date;  }
 }
+
