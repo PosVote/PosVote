a3b136506124ccf28c9a38c4abb777880cddc675
oddnine <dnwnsgur12@gmail.com>
2023-06-02 21:51:15 +0900

    어드민 아이디

--- src/main/java/study/postvote/util/StaticStr.java
+++ src/main/java/study/postvote/util/StaticStr.java
@@ -2,4 +2,6 @@ package study.postvote.util;
 
 public class StaticStr {
     public static String SERVER_IP = "localhost:8080";
+    public static String ADMIN_EMAIL = "sudo@admin.com";
+    public static String ADMIN_PASSWORD = "123";
 }
